2314ed35da3170bf5f8028d91291bd8ddefaac10
Oussama Mouhhaje <78048534+MOuhhaje@users.noreply.github.com>
2023-04-24 13:28:13 +0000

    Delete olds directory

--- olds/20-02-2023/resources/views/frontOffice/partialPages/chandelleHome.blade.php
+++
@@ -1,8 +0,0 @@
-  <!--==================== chandelle ====================-->
-   
-  <div class="wrapper ">
-      <div class="single-box"><img width="100%" alt="" src="{{ asset('imgs/chandelles.png') }}"></div>
-      <div class="single-box"><img width="100%" alt="" src="{{ asset('imgs/chandelles.png') }}"></div>
-      <div class="single-box"><img width="100%" alt="" src="{{ asset('imgs/chandelles.png') }}"></div>
-      <div class="single-box"><img width="100%" alt="" src="{{ asset('imgs/chandelles.png') }}"></div>
-  </div>
